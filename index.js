0b7c43ce35edda95152bc534779544ca4f3c0746
Daniel Armbruster <danielgundam00@gmail.com>
2022-02-23 09:25:36 -0700

    commit tree 214b494c3bce9017d562f734c2b290ea4afbb40a

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(where = "go to the office"){
+    return `This Monday, I will ${where}.`
+}
+
+
+let wrapAdjective = function(style="*"){
+    return function(adjective="speacail"){
+        return `You are ${style}${adjective}${style}!`
+    }
+} 
+
